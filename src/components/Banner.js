db8b78f8d39a3ad495dea385878d1b8d1ea068fc
Zya Nassurally <zya.nassurally@supinternet.fr>
2018-06-02 13:22:57 +0200

    changed the content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,9 @@ import SocialLinks from "./SocialLinks";
 const Banner = () => (
   <div className="row banner">
     <div className="banner-text">
-      <h1 className="responsive-headline">Je suis Zya Nassurally</h1>
+      <h1 className="responsive-headline">Zya Nassurally</h1>
       <h3>
-          développeur web <span>Front-end</span>.
+          Développeur web <span>Front-end</span>.
       </h3>
       <hr />
       <SocialLinks />
