8bab1bc84cc23cb141d3571ef54ad73a84636d5f
Zya Nassurally <zya.nassurally@supinternet.fr>
2018-06-04 20:52:38 +0200

    changed name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Ceevee",
-    author: "Aman Mittal",
-    description: "A Gatsby.js Starter based on Ceevee by Styleshout"
+    title: "Zya Nassurally",
+    author: "Zya Nassurally",
+    description: "Portfolio de Zya Nassurally"
   },
   pathPrefix: "/",
   plugins: [
